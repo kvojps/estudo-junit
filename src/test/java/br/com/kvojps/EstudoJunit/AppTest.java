a7fc1d01e8e28cfe433b69c4e67e5f8ebad51d58
Junior <josebazjunior09@gmail.com>
2022-04-12 13:34:53 -0300

    refatorando TestApp

--- src/test/java/br/com/kvojps/EstudoJunit/AppTest.java
+++ src/test/java/br/com/kvojps/EstudoJunit/AppTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertTrue;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -21,70 +20,66 @@ public class AppTest {
 
 	@BeforeAll
 	public static void setup() {
-		//TODO
-		//Executa antes de todos os casos de teste da classe
+		// TODO
+		// Executa antes de todos os casos de teste da classe
 	}
-	
+
 	@AfterAll
 	public static void done() {
-		//TODO
-		//Executa depois de todos os casos de teste
+		// TODO
+		// Executa depois de todos os casos de teste
 	}
-	
+
 	@BeforeEach
 	public void init() {
-		//TODO
-		//Executa antes de cada caso de teste da classe
+		// TODO
+		// Executa antes de cada caso de teste da classe
 	}
-	
-    @DisplayName("Single test sucessful")
+
+	@DisplayName("Single test sucessful")
 	@Test
-    public void deveResponderComTrue() {
-        assertTrue( true );
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    	//TODO
-    	//Executa depois de cada caso de teste
-    }
-    
-    @Test
-    @Disabled("Ainda não implementado")
-    public void deveMostrarSimplesAssertion() {
-    	Assertions.assertEquals(1, 1);
-    }
-    
-    
-    
-    @Test
-    public void deveValidarTodosItensDaLista() {
-    	List<Integer> list = Arrays.asList(2,3,5,7);
-    	Assertions.assertAll(() -> assertEquals(1, 1),
-    			() -> assertEquals(2, (int) list.get(0)),
-    			() -> assertEquals(3, (int) list.get(1)),
-    			() -> assertEquals(5, (int) list.get(2)),
-    			() -> assertEquals (7, (int) list.get(3)));
+	public void deveResponderComTrue() {
+		assertTrue(true);
+	}
+
+	@AfterEach
+	public void tearDown() {
+		// TODO
+		// Executa depois de cada caso de teste
+	}
+
+	@Test
+	@Disabled("Ainda não implementado")
+	public void deveMostrarSimplesAssertion() {
+		Assertions.assertEquals(1, 1);
+	}
+
+	@Test
+	public void deveValidarTodosItensDaLista() {
+		List<Integer> list = Arrays.asList(2, 3, 5, 7);
+		Assertions.assertAll(() -> assertEquals(1, 1), () -> assertEquals(2, (int) list.get(0)),
+				() -> assertEquals(3, (int) list.get(1)), () -> assertEquals(5, (int) list.get(2)),
+				() -> assertEquals(7, (int) list.get(3)));
 //    	Assertions.assertEquals(2, list.get(0));
 //    	Assertions.assertEquals(3, list.get(1));
 //    	Assertions.assertEquals(5, list.get(2));
 //    	Assertions.assertEquals(7, list.get(3));
-    }
-    
-    @Test
-    public void deveApenasExecutarTesteSeCriterioForAssumido() {
-    	int num = 5;
-    	//Executa o teste apenas se a assumption for verdadeira
-    	Assumptions.assumeTrue(num > 4);
-    	assertEquals(1, 1);
-    }
-    
-    @Test
-    public void assertThrowsException() {
-        String str = null;
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-          Integer.valueOf(str);
-        });
-    }
-    
+	}
+
+	@Test
+	public void deveApenasExecutarTesteSeCriterioForAssumido() {
+		int num = 5;
+		// Executa o teste apenas se a assumption for verdadeira
+		Assumptions.assumeTrue(num > 4);
+		assertEquals(1, 1);
+	}
+
+	@Test
+	public void assertThrowsException() {
+		String str = null;
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Integer.valueOf(str);
+		});
+	}
+
 }
