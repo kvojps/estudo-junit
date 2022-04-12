7752a4b63d90524b9d1e8e12c3aa66c6b23c8b7b
Junior <josebazjunior09@gmail.com>
2022-04-12 12:22:50 -0300

    adicionando conteudo sobre testes unitarios

--- src/test/java/br/com/kvojps/EstudoJunit/AppTest.java
+++ src/test/java/br/com/kvojps/EstudoJunit/AppTest.java
@@ -6,21 +6,56 @@ import static org.junit.Assert.assertTrue;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 
 public class AppTest {
 
-    @Test
+	@BeforeAll
+	public static void setup() {
+		//TODO
+		//Executa antes de todos os casos de teste da classe
+	}
+	
+	@AfterAll
+	public static void done() {
+		//TODO
+		//Executa depois de todos os casos de teste
+	}
+	
+	@BeforeEach
+	public void init() {
+		//TODO
+		//Executa antes de cada caso de teste da classe
+	}
+	
+    @DisplayName("Single test sucessful")
+	@Test
     public void deveResponderComTrue() {
         assertTrue( true );
     }
     
+    @AfterEach
+    public void tearDown() {
+    	//TODO
+    	//Executa depois de cada caso de teste
+    }
+    
     @Test
+    @Disabled("Ainda não implementado")
     public void deveMostrarSimplesAssertion() {
     	Assertions.assertEquals(1, 1);
     }
+    
+    
     
     @Test
     public void deveValidarTodosItensDaLista() {
@@ -42,6 +77,14 @@ public class AppTest {
     	//Executa o teste apenas se a assumption for verdadeira
     	Assumptions.assumeTrue(num > 4);
     	assertEquals(1, 1);
+    }
+    
+    @Test
+    public void assertThrowsException() {
+        String str = null;
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+          Integer.valueOf(str);
+        });
     }
     
 }
